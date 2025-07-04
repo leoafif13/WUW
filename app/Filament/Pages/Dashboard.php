c29b08553ec2abde5e06f1c65153d792aa6c73a0
Mufaaaa <faiq22836@gmail.com>
2025-07-04 17:56:29 +0700

    perubahan sedikit

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -10,6 +10,10 @@ class Dashboard extends Page
 
     protected static string $view = 'filament.pages.dashboard';
 
+    protected static ?string $navigationLabel = 'Beranda';
+    
+    protected static ?string $title = 'Beranda';
+
     // Tambahkan properti widgets untuk widget yang ingin dipakai
     protected function getWidgets(): array
     {
